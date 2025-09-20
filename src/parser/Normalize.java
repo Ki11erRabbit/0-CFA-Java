77da4202e297b9e42c8712c75da6102e3d776e66
ki11errabbit <ki11errabbit@posteo.net>
2025-09-20 11:18:56 -0600

    fixed an error in nested normalizing

--- src/parser/Normalize.java
+++ src/parser/Normalize.java
@@ -6,7 +6,7 @@ public class Normalize {
 
     public static ExpressionNode normalize(ExpressionNode node) {
         if (node instanceof Paren paren) {
-            return paren.getExpression();
+            return normalize(paren.getExpression());
         } else if (node instanceof Application app) {
             return new Application(normalize(app.getFunction()), normalize(app.getArgument()));
         } else if (node instanceof Lambda lambda) {
